f0e42ed3b4f3b8a03b03b89931d12477e389a169
David <david.markovic@elfak.rs>
2025-11-21 15:43:05 +0100

    Initial Commit

---
+++ KupujemProdajem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KupujemProdajem
+//
+//  Created by Macbook on 21/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
